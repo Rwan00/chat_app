8f3864d756071ecb7c52a3d7192170e78c357512
Rwan00 <93911923+Rwan00@users.noreply.github.com>
2024-03-22 01:54:25 +0200

    add sign up

--- lib/screens/auth.dart
+++ lib/screens/auth.dart
@@ -1,9 +1,12 @@
 import 'dart:developer';
 
 import 'package:chat_app/theme/fonts.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 import '../widgets/input_field.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -17,13 +20,33 @@ class _AuthScreenState extends State<AuthScreen> {
   bool _isLogin = true;
   var _enteredEmail = "";
   var _enteredPassword = "";
-  void _submit() {
+  void _submit() async {
     final valid = _formKey.currentState!.validate();
-    if (valid) {
-      _formKey.currentState!.save();
-      log(_enteredEmail);
-      log(_enteredPassword);
+    if (!valid) {
+      return;
     }
+    if (_isLogin) {
+      
+    }
+    
+    else{
+      try {
+        final UserCredential userCredential =
+            await _firebase.createUserWithEmailAndPassword(
+          email: _enteredEmail,
+          password: _enteredPassword,
+        );
+      }on FirebaseAuthException catch (e) {
+        ScaffoldMessenger.of(context).clearSnackBars();
+        ScaffoldMessenger.of(context).showSnackBar(
+          SnackBar(content: Text(e.message??"Authintication Failed!",style: titleStyle,),),
+        );
+      }
+    }
+
+    _formKey.currentState!.save();
+    log(_enteredEmail);
+    log(_enteredPassword);
   }
 
   @override
